0c9dc5859ad75e694433215388ab9b71c65f6904
tina1998612 <tina26919742@gmail.com>
2017-09-13 23:44:02 +0800

    use coupon, check balances functions complete in Coupon contract.

--- ECoupon/contracts/Coupon.sol
+++ ECoupon/contracts/Coupon.sol
@@ -6,6 +6,9 @@ contract Coupon is StandardToken {
   // Configuration
   string public constant name = "E-Coupon General Grant";
   string public constant symbol = "EGG";
+  string public startTime;
+  string public endTime;
+
   uint8 public constant decimals = 18;
   uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
 
@@ -14,6 +17,9 @@ contract Coupon is StandardToken {
 
   // Balance for each account
   mapping(address => uint256) balances;
+
+  // Balace used for each account
+  mapping(address => uint256) balancesUsed;
 
   // Approval of transfer
   mapping(address => mapping(address => uint256)) allowed;
@@ -26,20 +32,42 @@ contract Coupon is StandardToken {
     _;
   }
 
+  modifier isValidTime(){
+    require(startTime <= now);
+    require(endTime >= now);
+    _;
+  } 
+
   // Constructor
-  function Coupon() {
+  function Coupon(_startTime, _endTime) {
+    require(_startTime >= now);
+    require(_endTime >= _startTime);
+
     owner = msg.sender;
     balances[owner] = INITIAL_SUPPLY;
     totalSupply = INITIAL_SUPPLY;
+    startTime = _startTime;
+    endTime = _endTime;
   }
 
-  function totalSupply() constant returns (uint256 totalSupply) {
+  function totalSupply() public constant returns (uint256 totalSupply) {
     totalSupply = _totalSupply;
   }
 
+  function useCoupon(uint256 amount) public isValidTime returns (bool success) {
+    require(balances[msg.sender] >= amount);
 
+    balancesUsed[msg.sender].add(amount);
+    balances[msg.sender].sub(amount);
 
+    success = true; 
+  }
 
+  function checkBalancesUsed(address user) onlyOwner constant returns (uint256 _balancesUsed) {
+    _balancesUsed = balancesUsed[user];
+  }
 
-
+  function checkBalances(address user) onlyOwner constant returns (uint256 _balances) {
+    _balances = balances[user];
+  }
 }
