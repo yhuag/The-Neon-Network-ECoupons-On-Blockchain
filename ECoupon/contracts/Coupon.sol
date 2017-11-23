98b398ff620fecb6703c6eebc48da3f33e3794cf
Hu Yao-Chieh <yhuag@connect.ust.hk>
2017-11-23 15:27:41 +0800

    Update comments and trivial format

--- ECoupon/contracts/Coupon.sol
+++ ECoupon/contracts/Coupon.sol
@@ -9,8 +9,8 @@ contract Coupon is StandardToken {
   uint256 public startTime;
   uint256 public endTime;
 
-  uint8 public constant decimals = 18;
-  uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
+  uint8 public constant DECIMALS = 18;
+  uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(DECIMALS));
 
   // Owner of the contract
   address public owner;
@@ -32,6 +32,7 @@ contract Coupon is StandardToken {
     _;
   }
 
+  // Check whether the redeem time is between the span of startTime and endTime.
   modifier isValidRedeemTime(){
     require(startTime <= now);
     require(endTime >= now);
@@ -49,6 +50,7 @@ contract Coupon is StandardToken {
     endTime = _endTime;
   }
 
+  // Update the balances/balancesUsed of the user when coupon redeemed
   function redeem(uint256 amount) public isValidRedeemTime returns (bool success) {
     require(balances[msg.sender] >= amount);
 
@@ -58,18 +60,22 @@ contract Coupon is StandardToken {
     success = true; 
   }
 
+  // Only the owner can check the balance of a specific user
   function checkBalances(address user) onlyOwner constant returns (uint256 _balances) {
     _balances = balances[user];
   }
 
+  // Only the owner can check the balanceUsed of a specific user
   function checkBalancesUsed(address user) onlyOwner constant returns (uint256 _balancesUsed) {
     _balancesUsed = balancesUsed[user];
   }
 
+  // Anyone can check their personal balances
   function getAccountBalances() public constant returns (uint256 _balances) {
     _balances = balances[msg.sender];
   }
 
+  // Anyone can check their personal balances
   function getAccountBalancesUsed() public constant returns (uint256 _balances) {
     _balances = balancesUsed[msg.sender];
   }
