426a62687bff28abdce6d0f3db3096ea61483aac
tina1998612 <tina26919742@gmail.com>
2017-12-08 01:31:52 +0800

    coupon contract :)

---
+++ CouponMarket/contracts/Coupon.sol
@@ -0,0 +1,73 @@
+pragma solidity ^0.4.13;
+
+import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+
+contract Coupon is StandardToken {
+  // Configuration
+  uint256 ID;
+  address issuer;
+  address owner;
+  uint256 startTime;
+  uint256 endTime;
+  uint256 value;
+
+  modifier onlyOwner(){
+      assert(owner == msg.sender);
+      _;
+  }
+  // Functions with this modifier can only be executed by the issuer
+  modifier onlyIssuer() {
+    assert(issuer == msg.sender);
+    _;
+  }
+
+  // Check whether the redeem time is between the span of startTime and endTime.
+  modifier isValidRedeemTime(){
+    require(startTime <= now);
+    require(endTime >= now);
+    _;
+  } 
+
+  // Constructor: 
+  /**
+   * @param {int} ID - coupon ID.
+   */
+  function Coupon(uint256 id, uint256 _startTime, uint256 _endTime, uint256 _value) {
+    require(_endTime >= _startTime);
+
+    ID = id;
+    issuer =  msg.sender;
+    owner = msg.sender;
+    startTime =  _startTime;
+    endTime =  _endTime;
+    value =  _value;
+  }
+
+  // receiver is the owner to be changed to 
+  function changeOwner(address _receiver) onlyOwner() {
+      owner = _receiver;
+  }
+
+//   // Only the issuer can check the balance of a specific user
+//   function checkBalances(address user) public onlyIssuer constant returns (uint256 _balances) {
+//     _balances = balances[user];
+//   }
+
+
+//   // Only the issuer can check the balanceUsed of a specific user
+//   function checkBalancesUsed(address user) public onlyIssuer constant returns (uint256 _balancesUsed) {
+//     _balancesUsed = balancesUsed[user];
+//   }
+
+
+//   // Anyone can check their personal balances
+//   function getAccountBalances() public constant returns (uint256 _balances) {
+//     _balances = balances[msg.sender];
+//   }
+
+
+//   // Anyone can check their personal balances
+//   function getAccountBalancesUsed() public constant returns (uint256 _balances) {
+//     _balances = balancesUsed[msg.sender];
+//   }
+}
