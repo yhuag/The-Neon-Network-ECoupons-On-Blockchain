17a11d2fadf05887e732f562842c38ab64292394
tina1998612 <tina26919742@gmail.com>
2017-12-08 02:32:43 +0800

    coupon contract testing finished :)

--- CouponMarket/test/couponTest.js
+++ CouponMarket/test/couponTest.js
@@ -1,7 +1,7 @@
 var Coupon = artifacts.require("./Coupon.sol");
 var Market = artifacts.require("./Market.sol");
 
-contract('Coupon', function(accounts) {
+contract('Market', function(accounts) {
   it("Accessibility Test", function() {
   });
 
@@ -11,5 +11,34 @@ contract('Coupon', function(accounts) {
     assert.equal(0, volume, "volume is not zero");
   });
 
+  
+});
+
+contract('Coupon', function(accounts) {
+
+  it("Coupon Instantiation Test", async function() {
+    let coupon = await Coupon.new(0, 0, 10, 10);
+    var ID = await coupon.ID.call();
+    var startTime = await coupon.startTime.call();
+    var value = await coupon.value.call();
+    assert.equal(0, ID, "ID incorrect");
+    assert.equal(0, startTime, "startTime incorrect");
+    assert.equal(10, value, "value incorrect");
+  });
+
+  it("redeem", async function(){
+    let coupon = await Coupon.new(0, 0, 10, 10);
+    coupon.redeem();
+    var owner = await coupon.owner.call();
+    var issuer = await coupon.issuer.call();
+    assert.equal(issuer, owner, "owner incorrect");
+  });
+
+  it("transfer", async function(){
+    let coupon = await Coupon.new(0, 0, 10, 10);
+    coupon.transfer(accounts[1]);
+    var owner = await coupon.owner.call();
+    assert.equal(accounts[1], owner, "owner incorrect");
+  });
   
 });
