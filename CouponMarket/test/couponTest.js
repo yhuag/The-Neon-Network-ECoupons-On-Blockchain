f742854e6864685dafec930a11897408598687f8
Hu Yao-Chieh <yhuag@connect.ust.hk>
2017-12-08 02:06:35 +0800

    create test

--- CouponMarket/test/couponTest.js
+++ CouponMarket/test/couponTest.js
@@ -2,7 +2,14 @@ var Coupon = artifacts.require("./Coupon.sol");
 var Market = artifacts.require("./Market.sol");
 
 contract('Coupon', function(accounts) {
-  it("initial test", function() {
-    
+  it("Accessibility Test", function() {
   });
+
+  it("Market Instantiation Test", async function() {
+    let market = await Market.new();
+    var volume = await market.volume.call();
+    assert.equal(0, volume, "volume is not zero");
+  });
+
+  
 });
