9898493b7deba7cfc6414a2ec761c7c67152cf70
Hu Yao-Chieh <yhuag@connect.ust.hk>
2017-12-09 03:13:01 +0800

    minor

--- CouponMarket/CouponMarket_demo/app/javascripts/app.js
+++ CouponMarket/CouponMarket_demo/app/javascripts/app.js
@@ -213,6 +213,7 @@ window.addEventListener('load', async function () {
     coupon_info.value = await App.getCouponValue(_couponID);
     coupon_info.startTime = await App.getStartTime(_couponID);
     coupon_info.endTime = await App.getEndTime(_couponID);
+    
     appendCouponInfo(coupon_info);
   }
 
@@ -235,19 +236,6 @@ window.addEventListener('load', async function () {
     if(success) {
       appendUpdatedCouponInfoByID(couponID);
     }
-
-    // var coupon_info = {};
-    // coupon_info.ID = couponID;
-
-    // if(success) {
-    //   coupon_info.owner = await App.getOwnerAddr(couponID);      
-    //   coupon_info.issuer = await App.getIssuerAddr(couponID);
-    //   coupon_info.value = await App.getCouponValue(couponID);
-    //   coupon_info.startTime = await App.getStartTime(couponID);
-    //   coupon_info.endTime = await App.getEndTime(couponID);
-    //   appendCouponInfo(coupon_info);
-    // }
-
   });
 
   $('#redeem').click(async function () {
