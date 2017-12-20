f318cb3ca59641878446ecd7b9c90abba211963a
Hu Yao-Chieh <yhuag@connect.ust.hk>
2017-12-20 22:55:53 +0800

    huge migrate

--- CouponMarket/CouponMarket_demo/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var Coupon = artifacts.require("./Coupon.sol");
-var Market = artifacts.require("./Market.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Coupon);
-  deployer.deploy(Market);
-};
